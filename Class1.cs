25e0ba0ec1a5a398587a0e8ebe6447738b60640e
ST33-217A\Student <ryszard.kuczowic@chorzow.merito.pl>
2024-11-24 11:35:31 +0100

    sys pozycji

--- Class1.cs
+++ Class1.cs
@@ -4,5 +4,23 @@ namespace snakeGame
 {
     public class Class1
     {
+
+    }
+    public class Pos
+    {
+        public int x, y;
+    }
+    public class Playa
+    {
+        public Pos pos { get; set; }
+    }
+    public class Obstake
+    {
+
+        public int Class { get; set; }
+        public Pos Pos { get; set; }
+        // ?
+
+        public ConsoleColor ObstacleColor { get; set; }
     }
 }
